fb42aec7bd59c993ada1732ab4cca9b44686e250
Santiago Cortez <sanfctz@gmail.com>
2018-10-21 02:14:18 -0300

    [Add] Project files
    - Simple samples of lambda expresions

---
+++ LambdaBasics/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LambdaBasics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //lista de parametros => instrucciones a realizar
+            //Se puede usar donde se haga uso de un metodo anonimo
+            //El tipo de parametros puede ser explicito o implicito
+
+            List<int> numeros = new List<int>();
+            numeros.AddRange(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
+
+            //Hacemos uso de la expresion lambda
+            List<int> numerosPar = numeros.FindAll(i => (i %2) == 0);
+
+            //Mostramos los números
+            foreach(int n in numerosPar)
+            {
+                Console.WriteLine(n);
+            }
+
+        }
+    }
+}
---
+++ LambdaBasics2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LambdaBasics2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numeros = new List<int>();
+            numeros.AddRange(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
+
+            //Hacemos una expresion lambda mas compleja
+            List<int> numerosPares = numeros.FindAll((i) =>
+                {
+                    if (i % 2 == 0)
+                    {
+                        Console.WriteLine("Es Par");
+                        return true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Es Impar");
+                        return false;
+                    }
+                }
+            );
+            //Mostramos los numeros
+            foreach (var n in numerosPares)
+            {
+                Console.WriteLine(n);
+            }
+        }
+    }
+}
